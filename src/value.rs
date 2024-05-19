21c96e060b2c9cccc011b820332378ed7efac3b9
blurrycat <123909576+blurrycat@users.noreply.github.com>
2024-05-19 17:43:36 +0200

    feat: simplify value calculation

--- src/value.rs
+++ src/value.rs
@@ -28,3 +28,23 @@ impl std::str::FromStr for BrightnessValue {
         }
     }
 }
+
+impl BrightnessValue {
+    pub fn calculate(&self, current: u32, max: u32) -> u32 {
+        match self {
+            BrightnessValue::Absolute(value) => (*value).min(max).max(0),
+            BrightnessValue::Relative(value) => {
+                let new = (current as i32 + value) as u32;
+                new.min(max).max(0)
+            }
+            BrightnessValue::Percent(value) => {
+                let new = (*value as f64 / 100.0 * max as f64) as u32;
+                new.min(max).max(0)
+            }
+            BrightnessValue::RelativePercent(value) => {
+                let new = (current as i32 + (*value as f64 / 100.0 * max as f64) as i32) as u32;
+                new.min(max).max(0)
+            }
+        }
+    }
+}
